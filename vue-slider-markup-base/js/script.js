ce867cb0d85432dc03fd48b3aa40b909b24e4a22
alessandrofogliada <fogliada.a@gmail.com>
2024-06-25 17:09:51 +0200

    1.0.1 Immagini e testi dinamici nel DOM

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -5,11 +5,10 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return{   
-
+        return {   
             slides: [
                 {
-                    image: 'img/01.webp',
+                        image: 'img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                     }, {
